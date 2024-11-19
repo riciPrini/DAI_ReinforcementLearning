5259e0a256f2e82ac84f364b62be3479417c477d
riccardo_prini00 <274292@studenti.unimore.it>
2024-11-19 15:01:32 +0100

    Fixes on 2x2 env regarding the wrong lane IDs

--- src/includes/DQAgent.py
+++ src/includes/DQAgent.py
@@ -80,31 +80,47 @@ class DQAgent(object):
             self.acc_wait_time = self.acc_wait_time[-3:]
 
         return self.reward
-
+    def vehicle_counter(self,vehicle_wait_time,road_id,lanes):
+                if road_id == lanes[0]: #North
+                    self.no_veh_N += 1 
+                    self.wait_time_N += vehicle_wait_time                    
+                elif road_id == lanes[1]: #East
+                    self.no_veh_E += 1 
+                    self.wait_time_E += vehicle_wait_time
+                elif road_id == lanes[2]: #West
+                    self.no_veh_W += 1 
+                    self.wait_time_W += vehicle_wait_time
+                elif road_id == lanes[3]: #South
+                    self.no_veh_S += 1 
+                    self.wait_time_S += vehicle_wait_time
     def get_avg_wait_time(self):
+        """
+        Function that return the avg wait time of vehicles in the traffic light. 
+        Regardless the 2x2 or 3x3 envirorment this still work due to the same number of lane IDs.
+
+        
+        """
         phase_central = traci.trafficlight.getRedYellowGreenState(self.TLC_name)
             ####
         for vehicle_id in traci.vehicle.getIDList():
             vehicle_speed = traci.vehicle.getSpeed(vehicle_id)
             road_id = traci.vehicle.getRoadID(vehicle_id)
             vehicle_wait_time = traci.vehicle.getAccumulatedWaitingTime(vehicle_id)
-            #print("Vehicle ID: ", vehicle_id, "Speed: ", vehicle_speed, "Road Id: ", road_id)
+            # print("TLC_name: ",self.TLC_name,"Vehicle ID: ", vehicle_id, "Speed: ", vehicle_speed, "Road Id: ", road_id)
 
             #Count vehicle at the TLC junction
             if vehicle_speed < 1: # Count only stoped vehicles
-                if   road_id == "eB_I": #North
-                    self.no_veh_N += 1 
-                    self.wait_time_N += vehicle_wait_time                    
-                elif road_id == "eD_I": #East
-                    self.no_veh_E += 1 
-                    self.wait_time_E += vehicle_wait_time
-                elif road_id == "eH_I": #West
-                    self.no_veh_W += 1 
-                    self.wait_time_W += vehicle_wait_time
-                elif road_id == "eF_I": #South
-                    self.no_veh_S += 1 
-                    self.wait_time_S += vehicle_wait_time
-
+                if self.TLC_name == "gneJ26":
+                    self.vehicle_counter(vehicle_wait_time,road_id,["eB_I","eD_I","eH_I","eF_I"]) #North,East,West,South
+                elif self.TLC_name == "gneJ27":
+                    self.vehicle_counter(vehicle_wait_time,road_id,["eA_H","eI_H","","eG_H"]) #North,East,West,South
+                elif self.TLC_name == "gneJ28":
+                    self.vehicle_counter(vehicle_wait_time,road_id,["eC_D","","eI_D","eE_D"]) #North,East,West,South
+                elif self.TLC_name == "gneJ29":
+                    self.vehicle_counter(vehicle_wait_time,road_id,["","eC_B","eA_B","eI_B"]) #North,East,West,South
+                elif self.TLC_name == "gneJ30":
+                    self.vehicle_counter(vehicle_wait_time,road_id,["eI_F","eE_F","eG_F",""]) #North,East,West,South
+        
         return stat.mean([self.wait_time_N, self.wait_time_E, self.wait_time_W, self.wait_time_S])
 
     def step(self, action, step):
@@ -153,18 +169,17 @@ class DQAgent(object):
 
             #Count vehicle at the TLC junction
             if vehicle_speed < 1: # Count only stoped vehicles
-                if   road_id == "eB_I": #North
-                    self.no_veh_N += 1 
-                    self.wait_time_N += vehicle_wait_time                    
-                elif road_id == "eD_I": #East
-                    self.no_veh_E += 1 
-                    self.wait_time_E += vehicle_wait_time
-                elif road_id == "eH_I": #West
-                    self.no_veh_W += 1 
-                    self.wait_time_W += vehicle_wait_time
-                elif road_id == "eF_I": #South
-                    self.no_veh_S += 1 
-                    self.wait_time_S += vehicle_wait_time
+                if self.TLC_name == "gneJ26":
+                    self.vehicle_counter(vehicle_wait_time,road_id,["eB_I","eD_I","eH_I","eF_I"]) #North,East,West,South
+                elif self.TLC_name == "gneJ27":
+                    self.vehicle_counter(vehicle_wait_time,road_id,["eA_H","eI_H","","eG_H"]) #North,East,West,South
+                elif self.TLC_name == "gneJ28":
+                    self.vehicle_counter(vehicle_wait_time,road_id,["eC_D","","eI_D","eE_D"]) #North,East,West,South
+                elif self.TLC_name == "gneJ29":
+                    self.vehicle_counter(vehicle_wait_time,road_id,["","eC_B","eA_B","eI_B"]) #North,East,West,South
+                elif self.TLC_name == "gneJ30":
+                    self.vehicle_counter(vehicle_wait_time,road_id,["eI_F","eE_F","eG_F",""]) #North,East,West,South
+            
 
         state_space = [self.wait_time_N, self.wait_time_E, self.wait_time_W, self.wait_time_S,
                  self.no_veh_N, self.no_veh_E, self.no_veh_W, self.no_veh_S]
